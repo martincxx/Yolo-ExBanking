fec3727013c87e7005b23f0c4db78572583e64eb
Martin Coronel <martincxx@gmail.com>
2023-06-12 15:14:16 +0300

    register user account

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,25 +1,24 @@
 import { rest } from "msw";
-import {users} from "./users.js";
-import { BASE_URL} from "../api/bank.js"
+import { users } from "./users.js";
+import { BASE_URL } from "../api/bank.js"
+import { createUserAccount } from "./util.js";
+
+const allUsers = users;
 
 export const handlers = [
   
   rest.post(`${BASE_URL}/users`, (req, res, ctx) => {
-    const user = req.body;
-    let allUsers = users;
+    const user = createUserAccount(JSON.parse(req.body));
     allUsers.push(user);
-
     return res(ctx.status(201));
   }),
 
   rest.get(`${BASE_URL}/users`, (req, res, ctx) => {
-    let allUsers = users;
     return res(ctx.status(200), ctx.json({ data: allUsers }));
   }),
 
   rest.get(`${BASE_URL}/users/:userci`, (req, res, ctx)=>{
     const {userci} = req.params
-    let allUsers = users;
     let user = allUsers.flatMap(user => user.ci == userci ? user: [])
     
     if (user.length ==1){
---
+++ src/mocks/util.js
@@ -0,0 +1,11 @@
+import { faker } from "@faker-js/faker";
+import { v4 as uuidv4 } from "uuid";
+
+export const createUserAccount = (a) => {
+  const user = { ...a };
+  user.account_number = faker.finance.accountNumber();
+  user.user_id = uuidv4();
+  user.status = "active";
+  user.timestamp = new Date().toISOString();
+  return user;
+};
