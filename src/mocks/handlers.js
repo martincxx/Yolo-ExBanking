9742f836a624df7725667c7c1d758d790e7dea05
Martin Coronel <martincxx@gmail.com>
2023-06-12 12:02:36 +0300

    create mock skeleton

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,14 +1,17 @@
 import { rest } from "msw";
-import users from "./users";
-
+import {users} from "./users.js";
+import { BASE_URL} from "../../api/bank.js"
 export const handlers = [
+    
+    
     rest.post("/users", null),
     
-    rest.get("/users", (req, res, ctx)=>{
+    rest.get(`${BASE_URL}/users`, (req, res, ctx)=>{
+        let allUsers = users;
         return res(
             ctx.status(200),
             ctx.json(
-                users
+                {data:allUsers}
             )
         )
     }),
---
+++ src/mocks/users.js
@@ -0,0 +1,104 @@
+export const users = [
+  {
+    "username": "Myah_Emard34",
+    "firstname": "Easter",
+    "lastname": "Monahan",
+    "account_number": "70285430",
+    "status": "active",
+    "user_id": "15fb6a75-c440-416a-bde8-03deac52d94b",
+    "ci": "PTWWCRGAW97",
+    "timestamp": "2021-09-11T00:15:06.476Z"
+  },
+  {
+    "username": "Joshuah82",
+    "firstname": "Broderick",
+    "lastname": "Howell",
+    "account_number": "91342025",
+    "status": "active",
+    "user_id": "a1083a9c-43f8-4f84-b071-491c30fa1f20",
+    "ci": "YIVPMOR2",
+    "timestamp": "2021-05-28T03:10:15.213Z"
+  },
+  {
+    "username": "Jovanny_Cassin",
+    "firstname": "Rae",
+    "lastname": "Wilderman",
+    "account_number": "89823681",
+    "status": "active",
+    "user_id": "92f5a6e2-b21a-4886-b389-5e7ce9b76b87",
+    "ci": "PMZWJMRBGNV",
+    "timestamp": "2023-08-09T22:11:37.307Z"
+  },
+  {
+    "username": "Leora_Gottlieb32",
+    "firstname": "Jayson",
+    "lastname": "Friesen",
+    "account_number": "39525342",
+    "status": "active",
+    "user_id": "6f1b389c-339c-41ed-9e43-79c835667109",
+    "ci": "EXGHPN5XC8I",
+    "timestamp": "2020-01-16T18:44:03.641Z"
+  },
+  {
+    "username": "Jackson.Hegmann",
+    "firstname": "Shirley",
+    "lastname": "Bernhard",
+    "account_number": "99216247",
+    "status": "active",
+    "user_id": "b92db0e7-ec46-48f9-9d99-f869ae1abb59",
+    "ci": "JAXBHN4Y",
+    "timestamp": "2017-06-23T19:48:59.117Z",
+    "id": "5"
+  },
+  {
+    "username": "Jamaal.Windler10",
+    "firstname": "Doris",
+    "lastname": "Keebler",
+    "account_number": "99091430",
+    "status": "active",
+    "user_id": "6ff3d2d3-0d11-4597-b585-eeaa7c576d73",
+    "ci": "NOLAUMF4",
+    "timestamp": "2015-09-27T05:16:53.044Z",
+    "id": "6"
+  },
+  {
+    "username": "Cristobal83",
+    "firstname": "Presley",
+    "lastname": "DuBuque",
+    "account_number": "13304928",
+    "status": "active",
+    "user_id": "f6811316-50aa-41a4-87c0-98d126f2ff00",
+    "ci": "XSRYDKQ6701",
+    "timestamp": "2022-02-27T21:26:13.398Z"
+  },
+  {
+    "username": "Ruben_Cole",
+    "firstname": "Lennie",
+    "lastname": "Senger",
+    "account_number": "79622453",
+    "status": "inactive",
+    "user_id": "5c8e438a-7433-4923-9419-dcdd4c588bbf",
+    "ci": "EJZKPARNTQC",
+    "timestamp": "2017-07-30T02:07:21.928Z"
+  },
+  {
+    "username": "Kayli.Klocko57",
+    "firstname": "Ima",
+    "lastname": "Feest",
+    "account_number": "28468326",
+    "status": "active",
+    "user_id": "b90f49db-a5c5-4ffc-88b4-b5422e955a4f",
+    "ci": "PHOXGYTNXXX",
+    "timestamp": "2019-02-08T16:09:30.518Z"
+  },
+  {
+    "username": "Brionna_Goyette",
+    "firstname": "Trey",
+    "lastname": "Powlowski",
+    "account_number": "11868695",
+    "status": "inactive",
+    "id": "cee9aa9a-3349-4598-b642-8daf79d09549",
+    "ci": "CHHSILD2XXX",
+    "timestamp": "2003-01-27T10:07:01.401Z"
+  }
+]
